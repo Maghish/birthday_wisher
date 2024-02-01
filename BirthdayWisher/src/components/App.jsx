f1941a960a36bbad5a6397ee693cf68341e541f4
Maghish <mail4mithran@gmail.com>
2024-02-01 14:23:45 +0530

    Integrated TailwindCSS

--- BirthdayWisher/src/components/App.jsx
+++ BirthdayWisher/src/components/App.jsx
@@ -1,9 +1,10 @@
+import '../css/index.css'
+
 import { useState } from 'react'
 
 function App() {
   return (
-    <>
-    </>
+    <p className='text-black text-5xl font-mono'>Happy Birthday Aswin Senpai!</p>
   )
 }
 
