8f6f5861038aa774d21dbeff9bfb7f5d88e8479f
Maghish <mail4mithran@gmail.com>
2024-02-01 14:37:58 +0530

    Integrated Firebase

---
+++ BirthdayWisher/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATWbH6OHM1kF-VUqv1PMxgS70So14R5co",
+  authDomain: "birthdaywisher-4b994.firebaseapp.com",
+  projectId: "birthdaywisher-4b994",
+  storageBucket: "birthdaywisher-4b994.appspot.com",
+  messagingSenderId: "204803541905",
+  appId: "1:204803541905:web:e08fd6dd7244813ed62830"
+};
+
+
+const app = initializeApp(firebaseConfig);
